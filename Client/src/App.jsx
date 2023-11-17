c324f5f0ddef8cd753e5da12c13be4c8127d570c
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-11-17 15:09:06 +0530

    latest updates

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -39,35 +39,27 @@ function App() {
   return (
     <>
       <div className="flex-1 flex flex-col overflow-hidden">
-        <Navbar />
-        <div className="app flex h-screen">
-          <div className="mr-24">
-            <Sidebar />
-          </div>
-          <main className="flex-1 overflow-x-hidden overflow-y-auto">
-            <div className="container mx-auto px-6 py-8">
-              <div className="flex flex-wrap -mx-4">
-                <div className="w-full md:w-2/3 lg:w-3/4 px-4">
-                  {/* Your main content goes here */}
-                  <div className="mx-auto text-center mb-10">
-                    <Stat />
-                  </div>
-                  <div className="card max-w-screen-md mx-auto bg-base-100 shadow-xl">
-                    <div className="card-body">
-                      <h2 className="card-title">Card title!</h2>
-                      <div className="w-full">
-                        <Linechart chartData={userData} className="w-full" />
-                      </div>
+        <Navbar />          
+        <main className="flex-1 overflow-x-hidden overflow-y-auto bg-base-300">
+          <div className="container mx-auto px-6 py-8">
+            <div className="flex flex-wrap -mx-4 justify-center">
+              <div className="w-full md:w-2/3 lg:w-3/4 px-4">
+                {/* Your main content goes here */}
+                <div className="mx-auto text-center mb-10">
+                  <Stat />
+                </div>
+                <div className="card max-w-screen-md mx-auto bg-base-100 shadow-xl">
+                  <div className="card-body">
+                    <h2 className="card-title">Card title!</h2>
+                    <div className="w-full">
+                      <Linechart chartData={userData} className="w-full" />
                     </div>
                   </div>
                 </div>
-                <div className="w-full md:w-1/3 lg:w-1/4 px-4">
-                  {/* Sidebar content goes here */}
-                </div>
               </div>
             </div>
-          </main>
-        </div>
+          </div>
+        </main>
       </div>
     </>
   );
