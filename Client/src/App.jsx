d441ae4530d92ef236b6e6b6baf98893789ee176
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-11-18 13:49:08 +0530

    housekeeping

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,78 +1,23 @@
-import { useState } from "react";
-
-// Import Data
-import { UserData } from "./Data/data";
-
 // Import UI
 import Navbar from "./components/basic_ui/Navbar";
 import Sidebar from "./components/basic_ui/Sidebar";
-import Linechart from "./components/other_ui/Linechart";
-import Stat from "./components/other_ui/Stat";
 
 // Import Routing
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Task from "./pages/Task";
+import Home from "./pages/Home"
 
 function App() {
-  const [userData, setUserData] = useState({
-    labels: UserData.map((data) => data.year),
-    datasets: [
-      {
-        label: "Users Gained",
-        data: UserData.map((data) => data.userGain),
-        fill: false,
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-          "rgba(255, 205, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(201, 203, 207, 0.2)",
-        ],
-        borderColor: [
-          "rgb(255, 99, 132)",
-          "rgb(255, 159, 64)",
-          "rgb(255, 205, 86)",
-          "rgb(75, 192, 192)",
-          "rgb(54, 162, 235)",
-          "rgb(153, 102, 255)",
-          "rgb(201, 203, 207)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  });
-
   return (
     <>
-    <BrowserRouter>
-      <div className="flex-1 flex flex-col overflow-hidden">
-        <Navbar />          
-        <Routes>
-          <Route path="/task" element={<Task />}/>
-        </Routes>
-        <main className="flex-1 overflow-x-hidden overflow-y-auto bg-base-300">
-          <div className="container mx-auto px-6 py-8">
-            <div className="flex flex-wrap -mx-4 justify-center">
-              <div className="w-full md:w-2/3 lg:w-3/4 px-4">
-                {/* Your main content goes here */}
-                <div className="mx-auto text-center mb-10">
-                  <Stat />
-                </div>
-                <div className="card max-w-screen-md mx-auto bg-base-100 shadow-xl">
-                  <div className="card-body">
-                    <h2 className="card-title">Card title!</h2>
-                    <div className="w-full">
-                      <Linechart chartData={userData} className="w-full" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </main>
-      </div>
+      <BrowserRouter>
+        <div className="flex-1 flex flex-col overflow-hidden">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/task" element={<Task />} />
+          </Routes>
+        </div>
       </BrowserRouter>
     </>
   );
