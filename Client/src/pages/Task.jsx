9ad22b78391fb134b61e9d0197c1baef4a074ecb
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-11-18 14:16:00 +0530

    task ui

--- Client/src/pages/Task.jsx
+++ Client/src/pages/Task.jsx
@@ -1,24 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const Task = () => {
   return (
     <div>
-        <main className="flex-1 overflow-x-hidden overflow-y-auto bg-base-300">
-        <div className="container mx-auto px-6 py-8">
-          <div className="flex flex-wrap -mx-4 justify-center">
-            <div className="w-full md:w-2/3 lg:w-3/4 px-4">
-              {/* Your main content goes here */}
-              <div className="card max-w-screen-md mx-auto bg-base-100 shadow-xl">
-                <div className="card-body">
-                  <h2 className="card-title">Card title!</h2>
-                </div>
+      <main className="flex-1 overflow-x-hidden overflow-y-auto bg-base-300">
+        <div role="tablist" className="tabs tabs-bordered max-w-screen-md mx-auto">
+          <input
+            type="radio"
+            name="my_tabs_1"
+            role="tab"
+            className="tab"
+            aria-label="Add to list"
+          />
+          <div role="tabpanel" className="tab-content p-10">
+            create todo
+          </div>
+
+          <input
+            type="radio"
+            name="my_tabs_1"
+            role="tab"
+            className="tab"
+            aria-label="View list"
+            checked
+          />
+          <div role="tabpanel" className="tab-content p-10">
+          <div className="card bg-base-100 shadow-xl">
+              <div className="card-body">
+                <h2 className="card-title">Card title!</h2>
               </div>
             </div>
           </div>
         </div>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Task
+export default Task;
