de6776e81fd19303c3e3e802915229a213fb5e60
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-11-18 14:43:18 +0530

    task tab nav

--- Client/src/pages/Task.jsx
+++ Client/src/pages/Task.jsx
@@ -1,37 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Task = () => {
+  const [activeTab, setActiveTab] = useState(1);
+
+  const handleTabClick = (tabNumber) => {
+    setActiveTab(tabNumber);
+  };
+
   return (
     <div>
-      <main className="flex-1 overflow-x-hidden overflow-y-auto bg-base-300">
-        <div role="tablist" className="tabs tabs-bordered max-w-screen-md mx-auto">
-          <input
-            type="radio"
-            name="my_tabs_1"
-            role="tab"
-            className="tab"
-            aria-label="Add to list"
-          />
-          <div role="tabpanel" className="tab-content p-10">
-            create todo
-          </div>
-
-          <input
-            type="radio"
-            name="my_tabs_1"
-            role="tab"
-            className="tab"
-            aria-label="View list"
-            checked
-          />
-          <div role="tabpanel" className="tab-content p-10">
-          <div className="card bg-base-100 shadow-xl">
-              <div className="card-body">
-                <h2 className="card-title">Card title!</h2>
-              </div>
-            </div>
+      <main className="overflow-x-hidden overflow-y-auto bg-base-300">
+        <div className="flex items-center justify-center mt-5">
+          <div role="tablist" className="tabs tabs-boxed">
+            <a
+              role="tab"
+              className={`tab ${activeTab === 1 ? "tab-active" : ""}`}
+              onClick={() => handleTabClick(1)}
+            >
+              View list
+            </a>
+            <a
+              role="tab"
+              className={`tab ${activeTab === 2 ? "tab-active" : ""}`}
+              onClick={() => handleTabClick(2)}
+            >
+              Add to list
+            </a>
           </div>
         </div>
+
+        {activeTab === 1 && <div>Content for Tab 1</div>}
+        {activeTab === 2 && <div>Content for Tab 2</div>}
+      
       </main>
     </div>
   );
