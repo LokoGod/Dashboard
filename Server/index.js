705be3826b81b8dac440eae6ffaa905cce98c124
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-11-19 21:23:44 +0530

    server

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,25 @@
 import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import "dotenv/config";
 
+// Instances
 const app = express();
+
+const port = process.env.PORT || 9000;
+
+// Server spin-up
+const start = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
 
 app.get("/", (req, res) => {
   res.send("Hello, there!");
