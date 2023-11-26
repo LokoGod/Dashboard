b9b5027d76b477f7939527cb18017de984ecdf9e
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-11-26 22:34:44 +0530

    model

---
+++ Server/models/categoryModel.js
@@ -0,0 +1,16 @@
+import connection from "../database/DBconfig";
+
+const categoryModel = {
+    getcategory: (callback) => {
+        connection.query("SELECT category_name FROM categories", (error, results) => {
+            if (error) {
+                console.error(error);
+                callback(error, null)
+            } else {
+                callback(null, results)
+            }
+        })
+    }
+}
+
+export default categoryModel
