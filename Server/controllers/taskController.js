55a7609baaacd48643ef0b95b7c2b9354dd537f0
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-11-27 23:38:31 +0530

    create

--- Server/controllers/taskController.js
+++ Server/controllers/taskController.js
@@ -1,24 +1,39 @@
 import taskModel from "../models/taskModel.js";
 
 const taskController = {
-    getTask: (req, res) => {
-        taskModel.getTask((err, results) => {
-            if(err) {
-                res.status(500).send("Internal Server Error")
-            } else {
-                res.status(200).json(results)
-            }
-        })
-    },
-    createTask: (req, res) => {
-        taskModel.createTask((err, results) => {
-            if(err) {
-                res.status(500).send("Internal server error when creating")
-            } else {
-                res.status(201).send(results)
-            }
-        })
-    }
-}
+  getTask: (req, res) => {
+    taskModel.getTask((err, results) => {
+      if (err) {
+        res.status(500).send("Internal Server Error");
+      } else {
+        res.status(200).json(results);
+      }
+    });
+  },
+  createTask: (req, res) => {
+    taskModel.createTask((err, results) => {
+      try {
+        const { category_id, state_id, summary, description } = req.body;
+        const taskId = taskModel.createTask({
+          category_id,
+          state_id,
+          summary,
+          description,
+        });
+        res.status(201).json({
+          success: true,
+          message: "Task created successfully",
+          data: { id: taskId, category_id, state_id, summary, description },
+        });
+      } catch (error) {
+        console.error("Error creating task:", error);
+        res.status(500).json({
+          success: false,
+          message: "Internal server error",
+        });
+      }
+    });
+  },
+};
 
-export default taskController
+export default taskController;
