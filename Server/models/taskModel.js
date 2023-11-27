33f5214c6ae2fab6cc4fb675bf71385f803d9658
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-11-27 23:13:37 +0530

    taskModel

--- Server/models/taskModel.js
+++ Server/models/taskModel.js
@@ -11,7 +11,16 @@ const taskModel = {
       }
     });
   },
-  
+  createTask: (callback) => {
+    connection.query("INSERT INTO task_list ('?', '?', '?', '?', '?')", (error, results) => {
+      if (error) {
+        console.error(error);
+        callback(error, null);
+      } else {
+        callback(null, results);
+      }
+    });
+  },
 };
 
 export default taskModel;
