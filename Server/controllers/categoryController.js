9e75dc8d21d18e3a3680c9b07320ee41553a30eb
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-11-27 19:52:41 +0530

    stateController

--- Server/controllers/categoryController.js
+++ Server/controllers/categoryController.js
@@ -1,15 +1,15 @@
 import categoryModel from "../models/categoryModel.js";
 
 const categoryController = {
-    getCategory: (req, res) => {
-        categoryModel.getCategory((err, results) => {
-            if (err) {
-                res.status(500).send("Internal Server Error")
-            } else {
-                res.status(200).json(results);
-            }
-        })
-    }
-}
+  getCategory: (req, res) => {
+    categoryModel.getCategory((err, results) => {
+      if (err) {
+        res.status(500).send("Internal Server Error");
+      } else {
+        res.status(200).json(results);
+      }
+    });
+  },
+};
 
-export default categoryController
+export default categoryController;
---
+++ Server/controllers/stateController.js
@@ -0,0 +1,15 @@
+import stateModel from "../models/stateModel";
+
+const stateController = {
+  getState: (req, res) => {
+    stateModel.getState((err, results) => {
+      if (err) {
+        res.status(500).send("Internal Server Error");
+      } else {
+        res.status(200).json(results);
+      }
+    });
+  },
+};
+
+export default stateController;
