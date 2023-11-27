2fba21ab7816e3b2137468bdcdf936543ceb1f50
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-11-27 23:20:30 +0530

    controller

---
+++ Server/controllers/taskController.js
@@ -0,0 +1,24 @@
+import taskModel from "../models/taskModel";
+
+const taskController = {
+    getTask: (req, res) => {
+        taskModel.getTask((err, results) => {
+            if(err) {
+                res.status(500).send("Internal Server Error")
+            } else {
+                res.status(200).json(results)
+            }
+        })
+    },
+    createTask: (req, res) => {
+        taskModel.createTask((err, results) => {
+            if(err) {
+                res.status(500).send("Internal server error when creating")
+            } else {
+                res.status(201).send(results)
+            }
+        })
+    }
+}
+
+export default taskController
