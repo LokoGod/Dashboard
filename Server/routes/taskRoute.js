130931bb9571f129042b0e6ee13bef3ca02109f2
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-11-29 17:59:13 +0530

    route optimization

--- Server/routes/taskRoute.js
+++ Server/routes/taskRoute.js
@@ -3,7 +3,6 @@ import taskController from "../controllers/taskController.js";
 
 const taskRouter = express.Router();
 
-taskRouter.get("/", taskController.getTask)
-taskRouter.post("/", taskController.createTask);
+taskRouter.get("/", taskController.getTask).post(taskController.createTask)
 
 export default taskRouter;
