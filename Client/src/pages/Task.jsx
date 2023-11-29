603145cd6b5544b9e95c74191cf0abc779d50af0
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-11-29 21:31:43 +0530

    cd

--- Client/src/pages/Task.jsx
+++ Client/src/pages/Task.jsx
@@ -5,6 +5,7 @@ const apiEndpoint = "http://localhost:5000/api/v1/";
 
 const Task = () => {
   const [activeTab, setActiveTab] = useState(1);
+  const [tasks, setTasks] = useState([]);
   const [categories, setCategories] = useState([]);
   const [states, setStates] = useState([]);
   const [formData, setFormData] = useState({
@@ -50,6 +51,18 @@ const Task = () => {
     fetchState();
   }, []);
 
+  useEffect(() => {
+    const fetchTask = async () => {
+      const response = await axios.get(`${apiEndpoint}task/`);
+      const updatedTasks = response.data.map((task) => {
+        const category = categories.find((cat) => cat.category_id === task.category_id);
+        return { ...task, category_name: category ? category.category_name : "" };
+      });
+      setTasks(updatedTasks);
+    };
+    fetchTask();
+  }, [categories]);
+
   return (
     <div className="min-h-screen">
       <main className="w-full overflow-x-hidden overflow-y-auto bg-base-300 p-5">
@@ -74,20 +87,21 @@ const Task = () => {
 
         {activeTab === 1 && (
           <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            {/* Card 1 */}
+            {tasks.map((task) => (    
             <div className="card bg-neutral text-neutral-content">
               <div className="card-body items-center text-center">
-                <h2 className="card-title">Dashboard project</h2>
-                <p>Connect with the API to finish what you started.</p>
+                <h2 className="card-title">{task.summary}</h2>
+                <p>{task.description}</p>
                 <div className="card-actions justify-end">
                   <button className="btn btn-primary">Finish</button>
                   <button className="btn btn-ghost">Ignore</button>
                 </div>
                 <div className="card-actions justify-end mt-2">
-                  <div className="badge badge-accent">Personal</div>
+                  <div className="badge badge-accent">{task.category_name}</div>
                 </div>
               </div>
             </div>
+            ))}
 
             {/* Add more cards as needed */}
           </div>
