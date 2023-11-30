5d42ef2a0954c247a583adcbd6cbd2e63a94ec3a
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-11-30 17:41:14 +0530

    refactoring taskModel

--- Server/models/taskModel.js
+++ Server/models/taskModel.js
@@ -3,50 +3,37 @@ import connection from "../database/DBconfig.js";
 const taskModel = {
   getTask: (callback) => {
     connection.query("SELECT * FROM task_list", (error, results) => {
-      if (error) {
-        console.error(error);
-        callback(error, null);
-      } else {
-        callback(null, results);
-      }
+      callback(error, results);
     });
   },
+
   createTask: (taskData, callback) => {
     const { category_id, state_id, summary, description } = taskData;
-
     connection.query(
       "INSERT INTO task_list (category_id, state_id, summary, description) VALUES (?, ?, ?, ?)",
-      [category_id, state_id, summary, description], // The array prevents SQL injection
+      [category_id, state_id, summary, description],
       (error, results) => {
-        if (error) {
-          console.error(error);
-          callback(error, null);
-        } else {
-          callback(null, results);
-        }
+        callback(error, results);
       }
     );
   },
-  getUniqueTask: (callback) => {
-    connection.query('SELECT * FROM task_list WHERE task_id = ?', (error, results) => {
-      if (error) {
-        console.error(error);
-        callback(error, null);
-      } else {
-        callback(null, results);
-      }
-    });
-  },
-  deleteTask: (callback) => {
+
+  getUniqueTask: (taskId, callback) => {
     connection.query(
-      'DELETE FROM task_list WHERE category_id = "?"',
+      "SELECT * FROM task_list WHERE task_id = ?",
+      [taskId],
       (error, results) => {
-        if (error) {
-          console.error(error);
-          callback(error, null);
-        } else {
-          callback(null, results);
-        }
+        callback(error, results);
+      }
+    );
+  },
+
+  deleteTask: (taskId, callback) => {
+    connection.query(
+      "DELETE FROM task_list WHERE task_id = ?",
+      [taskId],
+      (error, results) => {
+        callback(error, results);
       }
     );
   },
