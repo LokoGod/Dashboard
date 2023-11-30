09fb9b0a5cdfeeeb65a75b60ba1d3ce3110adc9b
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-11-30 19:43:58 +0530

    patch

--- Server/controllers/taskController.js
+++ Server/controllers/taskController.js
@@ -62,6 +62,37 @@ const taskController = {
     });
   },
 
+  completedTask: (req, res) => {
+    try {
+      const taskId = req.params.id;
+      const { completed } = req.body;
+
+      taskModel.completedTask(taskId, completed, (err, results) => {
+        if (err) {
+          console.error("Error updating task:", err);
+          res.status(500).json({
+            success: false,
+            message: "Internal server error",
+            error: err.message,
+          });
+        } else {
+          res.status(200).json({
+            success: true,
+            message: "Task status updated successfully",
+            data: results,
+          });
+        }
+      });
+    } catch (error) {
+      console.error("Error updating task:", error);
+      res.status(500).json({
+        success: false,
+        message: "Internal server error",
+        error: error.message,
+      });
+    }
+  },
+
   deleteTask: (req, res) => {
     const taskId = req.params.id; // Extract taskId from params
 
--- Server/routes/taskRoute.js
+++ Server/routes/taskRoute.js
@@ -7,5 +7,6 @@ taskRouter.get("/", taskController.getTask);
 taskRouter.post("/", taskController.createTask);
 taskRouter.delete("/:id", taskController.deleteTask);
 taskRouter.get("/:id", taskController.getUniqueTask);
+taskRouter.patch("/:id", taskController.completedTask);
 
 export default taskRouter;
