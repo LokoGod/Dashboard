49a1f6b85f524cef6090487aa9fab5d8bc4230ff
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-01 00:22:41 +0530

    refactoring

--- Server/routes/taskRoute.js
+++ Server/routes/taskRoute.js
@@ -3,10 +3,16 @@ import taskController from "../controllers/taskController.js";
 
 const taskRouter = express.Router();
 
-taskRouter.get("/", taskController.getTask);
-taskRouter.post("/", taskController.createTask);
-taskRouter.delete("/:id", taskController.deleteTask);
-taskRouter.get("/:id", taskController.getUniqueTask);
+taskRouter
+  .route("/")
+  .get(taskController.getTask)
+  .post(taskController.createTask);
+
+taskRouter
+  .route("/:id")
+  .get(taskController.getUniqueTask)
+  .delete(taskController.deleteTask);
+
 taskRouter.patch("/:id", taskController.completedTask);
 
 export default taskRouter;
