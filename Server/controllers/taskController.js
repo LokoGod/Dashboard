534c36d2e8a4397d04cb836e4b84cac9d501fa51
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-11-30 17:52:35 +0530

    refactor

--- Server/controllers/taskController.js
+++ Server/controllers/taskController.js
@@ -1,13 +1,15 @@
-import taskModel from '../models/taskModel.js';
+import taskModel from "../models/taskModel.js";
 
 const taskController = {
-  getTasks: (req, res) => {
+  getTask: (req, res) => {
     taskModel.getTask((err, results) => {
       if (err) {
-        console.error("Error fetching tasks:", err);
-        res.status(500).json({ success: false, message: "Internal Server Error" });
+        res.status(500).json({
+          success: false,
+          message: "Internal Server Error",
+        });
       } else {
-        res.status(200).json({ success: true, data: results });
+        res.status(200).json(results);
       }
     });
   },
@@ -16,55 +18,60 @@ const taskController = {
     try {
       const { category_id, state_id, summary, description } = req.body;
 
-      taskModel.createTask({ category_id, state_id, summary, description }, (err, results) => {
-        if (err) {
-          console.error("Error creating task:", err);
-          res.status(500).json({ success: false, message: "Internal Server Error" });
-        } else {
-          const taskId = results.insertId;
-          res.status(201).json({
-            success: true,
-            message: "Task created successfully",
-            data: { id: taskId, category_id, state_id, summary, description },
-          });
+      taskModel.createTask(
+        { category_id, state_id, summary, description },
+        (err, results) => {
+          if (err) {
+            console.error("Error creating task:", err);
+            res.status(500).json({
+              success: false,
+              message: "Internal server error",
+            });
+          } else {
+            const taskId = results.insertId;
+            res.status(201).json({
+              success: true,
+              message: "Task created successfully",
+              data: { id: taskId, category_id, state_id, summary, description },
+            });
+          }
         }
-      });
+      );
     } catch (error) {
       console.error("Error creating task:", error);
-      res.status(500).json({ success: false, message: "Internal Server Error" });
+      res.status(500).json({
+        success: false,
+        message: "Internal server error",
+      });
     }
   },
 
   getUniqueTask: (req, res) => {
     const taskId = req.params.id;
 
-    taskModel.getUniqueTask(taskId, (err, results) => {
+    taskModel.getUniqueTask(taskId, (err, results) => { // Corrected function name
       if (err) {
-        console.error("Error fetching unique task:", err);
-        res.status(500).json({ success: false, message: "Internal Server Error" });
+        res.status(500).json({
+          success: false,
+          message: "Internal server error",
+        });
       } else {
-        if (!results || results.length === 0) {
-          res.status(404).json({ success: false, message: "Task not found" });
-        } else {
-          res.status(200).json({ success: true, data: results[0] });
-        }
+        res.status(200).json(results);
       }
     });
   },
 
   deleteTask: (req, res) => {
-    const taskId = req.params.id;
+    const taskId = req.params.id; // Extract taskId from params
 
-    taskModel.deleteTask(taskId, (err, results) => {
+    taskModel.deleteTask(taskId, (err, results) => { // Corrected function parameters
       if (err) {
-        console.error("Error deleting task:", err);
-        res.status(500).json({ success: false, message: "Internal Server Error" });
+        res.status(500).json({
+          success: false,
+          message: "Internal server error",
+        });
       } else {
-        if (results.affectedRows === 0) {
-          res.status(404).json({ success: false, message: "Task not found" });
-        } else {
-          res.status(204).send(); // No content for successful deletion
-        }
+        res.status(200).json(results);
       }
     });
   },
